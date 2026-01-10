67cee9a1f156dc15b8d4e30c9559b67764c67937
shubhanshu-a32 <osmwow17@gmail.com>
2026-01-10 23:46:25 +0530

    Enforce b2c_marketplace database connection

--- src/config/db.js
+++ src/config/db.js
@@ -3,14 +3,15 @@ const mongoose = require('mongoose');
 const connectDB = async (uri) => {
     try {
         await mongoose.connect(uri, {
+            dbName: 'b2c_marketplace',
             serverSelectionTimeoutMS: 5000
         });
         mongoose.set('bufferCommands', false);
         console.log('🚀 MongoDB connected');
-    } catch(err) {
+    } catch (err) {
         console.log('MongoDB connection error:', err);
         throw err;
     }
 };
 
-module.exports = {connectDB};
+module.exports = { connectDB };
