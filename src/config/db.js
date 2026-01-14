bfca6a4ba251b628ec7e01e8373f2ca591b370cf
shubhanshu-a32 <osmwow17@gmail.com>
2026-01-14 18:29:26 +0530

    New Codes

--- src/config/db.js
+++ src/config/db.js
@@ -1,22 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const connectDB = async (uri) => {
-//     try {
-//         await mongoose.connect(uri, {
-//             dbName: 'b2c_marketplace',
-//             serverSelectionTimeoutMS: 5000
-//         });
-//         mongoose.set('bufferCommands', false);
-//         console.log('🚀 MongoDB connected');
-//     } catch (err) {
-//         console.log('MongoDB connection error:', err);
-//         throw err;
-//     }
-// };
-
-// module.exports = { connectDB };
-
-// src/config/db.js
 'use strict';
 
 const mongoose = require('mongoose');
